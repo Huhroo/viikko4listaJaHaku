98b15e0c585ec361d4d56e79325ca06556cde81d
Huhroo <roope.v.huhta@gmail.com>
2024-02-05 13:24:05 +0200

    inti

--- App.js
+++ App.js
@@ -1,12 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, SafeAreaView, Platform, StyleSheet, Text, StatusBar } from 'react-native';
+import { DATA } from './Data';
+import Constants from 'expo-constants';
+import Row from './components/Row';
+import Search from './components/Search';
+import { useEffect, useState } from 'react';
 
 export default function App() {
+  const [items,setItems] = useState([]);
+
+  useEffect(() => {
+    setItems(DATA);
+  }, [])
+    
+  const executeSearch = (search) => {
+    const searchArray = DATA.filter((item) => item.lastname.startsWith(search));
+    setItems(searchArray);
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Search executeSearch={executeSearch}/>
+      <FlatList
+         data={items}
+         renderItem={({item}) => (
+          <Row person={item} />
+         )}
+      ></FlatList>
+    </SafeAreaView>
   );
 }
 
@@ -14,7 +34,9 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: 'left',
+    justifyContent: 'left',
+    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight + Constants.statusBarHeight : 0,
+    
   },
 });
---
+++ Data.js
@@ -0,0 +1,78 @@
+export const DATA = [
+    {
+        id: '1',
+        lastname: 'Alppi',
+        firstname: 'Veijari'
+    },
+    {
+        id: '2',
+        lastname: 'Bolkka',
+        firstname: 'Heijari'
+    },
+    {
+        id: '3',
+        lastname: 'Chaolta',
+        firstname: 'Beijari'
+    },
+    {
+        id: '4',
+        lastname: 'Deolta',
+        firstname: 'Zeijari'
+    },
+    {
+        id: '5',
+        lastname: 'Ernesti',
+        firstname: 'Xeijari'
+    },
+    {
+        id: '6',
+        lastname: 'Foresti',
+        firstname: 'Ceijari'
+    },
+    {
+        id: '7',
+        lastname: 'Geikka',
+        firstname: 'Deijari'
+    },
+    {
+        id: '8',
+        lastname: 'Heikka',
+        firstname: 'Weijari'
+    },
+    {
+        id: '9',
+        lastname: 'Ilmor',
+        firstname: 'Qeijari'
+    },
+    {
+        id: '10',
+        lastname: 'Jaffa',
+        firstname: 'Teijari'
+    },
+    {
+        id: '11',
+        lastname: 'Kelppi',
+        firstname: 'Reijari'
+    },
+    {
+        id: '12',
+        lastname: 'Lispi',
+        firstname: 'Keijari'
+    },
+    {
+        id: '13',
+        lastname: 'Moltes',
+        firstname: 'Geijari'
+    },
+    {
+        id: '14',
+        lastname: 'Narwhal',
+        firstname: 'Feijari'
+    }, 
+    {
+        id: '15',
+        lastname: 'Olppi',
+        firstname: 'Leijari'
+    }
+
+]
